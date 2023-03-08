814ecede648d5cbf2963e32c55f04927cf9b055d
오민택 <ohht10004@gmail.com>
2023-03-08 20:40:01 +0900

    Edit code and delete unnecessary part. Add save mode

--- BEV_main.py
+++ BEV_main.py
@@ -23,17 +23,35 @@ import rospy
 import cv_bridge
 from sensor_msgs.msg import Image
 import util.BEV as bev
+import argparse
+import datetime
 
 actor_list = []
 IM_WIDTH = 1024
 IM_HEIGHT = 1024
 
-def process_img(image,pub):
+def publish_img(image,pub):
     i = np.ndarray(shape=((image.height, image.width, 4)), dtype=np.uint8, buffer=image.raw_data)[:, :, 0:3]
     i = bev.main(i)
     pub.publish(cv_bridge.CvBridge().cv2_to_imgmsg(np.array(i), encoding='passthrough'))
-    #print('Working...')
+    
+def save_img(image, directory, suffix):
+    i = np.ndarray(shape=((image.height, image.width, 4)), dtype=np.uint8, buffer=image.raw_data)[:, :, 0:3]
+    i = bev.main(i)
+    ### 저장 코드 넣기
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+    path = f'{directory}/{str(suffix)}.jpg'
+    cv2.imwrite(path, i)
 
+suffix = 0
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--save', default=True, help = 'Save BEV image (default = True)')
+parser.add_argument('--publish', default=False, help='Publish BEV image (default = False, Default Topic = Image/BEV/Front)')
+args = parser.parse_args()
+start = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
 try:
     client = carla.Client('localhost', 2000)
     client.set_timeout(20)
@@ -45,14 +63,14 @@ try:
     settings.fixed_delta_seconds = 0.05
     world.apply_settings(settings)
 
-    rospy.init_node('Publisher')
-    pub = rospy.Publisher('Image/BEV/Front', Image)
+    if args.publish:
+        rospy.init_node('Publisher')
+        pub = rospy.Publisher('Image/BEV/Front', Image)
 
 
     blueprint_library = world.get_blueprint_library()
 
     bp = blueprint_library.filter('model3')[0]
-    print(bp)
 
     spawn_point = random.choice(world.get_map().get_spawn_points())
     vehicle = world.spawn_actor(bp, spawn_point)
@@ -78,10 +96,17 @@ try:
     # add sensor to list of actors
     actor_list.append(sensor)
     # do something with this sensor
-    sensor.listen(lambda data: process_img(data, pub))
+
+    if args.publish:
+        sensor.listen(lambda data: publish_img(data, pub))
+    elif args.save:
+        sensor.listen(lambda data: save_img(data, start, suffix = suffix))
 
     while True:
         world.tick()
+        if world.tick():
+            suffix += 1
+
         #vehicle.set_autopilot(True)  # if you just wanted some NPCs to drive.
 
 finally:
--- util/BEV.py
+++ util/BEV.py
@@ -14,20 +14,16 @@ def on_line(p1, p2, ycoord):
 def main(img, debugging = False):
     img_original = img.copy()
 
-    # set center position
     mean_position = [0,0]
     mean_position[1], mean_position[0], _ = img.shape
-
-    #print(mean_position)
 
     mean_position[1] = mean_position[1] / 2
     mean_position[0] = mean_position[0] / 2
 
-    #temp_img = cv2.imread('another_yolo.jpg')
-    top = mean_position[1] + 20 # 소멸점보다 20픽셀 아래를 잡는다
+    top = mean_position[1] + 20
     bottom,width,_=img.shape
-    bottom = bottom + 0# 차량의 앞부분이 거의 나오지 않는 본넷시점이라면 사용
-    width = int(width*2 / 5) # 위쪽 폭은 이미지 전체폭의 2/5
+    bottom = bottom + 0
+    width = int(width*2 / 5)
     p1 = [int(mean_position[0] - width/2), top]
     p2 = [int(mean_position[0] + width/2), top]
     p3 = on_line(p2, mean_position, bottom)
